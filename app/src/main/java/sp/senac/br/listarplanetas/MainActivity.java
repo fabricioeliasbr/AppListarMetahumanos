8d23474e9a4fb37e9933408759b63e90583d7f87
fabricioeliasbr <fabricio.silva226@etec.sp.gov.br>
2019-08-05 22:18:13 -0300

    First commit.

---
+++ app/src/main/java/sp/senac/br/listarplanetas/MainActivity.java
@@ -0,0 +1,74 @@
+package sp.senac.br.listarplanetas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    String[] namePlanets = {"Mercurio", "Batman", "Ciclope", "Colosso", "Deadpool", "Feiticeira", "Flash",
+            "Homem-formiga", "Homem-aranha", "Jean Grey", "Tempestade", "Thor", "Tocha-humana"};
+    int[] imagePlanets = {R.drawable.mercurio, R.drawable.batma, R.drawable.ciclope, R.drawable.colosso, R.drawable.deadpool, R.drawable.feiticeira,
+            R.drawable.flash, R.drawable.homiformiga, R.drawable.homiranha, R.drawable.jeangrey, R.drawable.tempestade, R.drawable.thor, R.drawable.tochahumana};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.lstPlanetas);
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),ListarDadosPlanetasActivity.class);
+                intent.putExtra("name",namePlanets[position]);
+                intent.putExtra("image", imagePlanets[position]);
+                startActivity(intent);
+            }
+        });
+    }
+
+    class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return imagePlanets.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View view1 = getLayoutInflater().inflate(R.layout.carrega_planetas, null);
+
+            TextView name = (TextView) view1.findViewById(R.id.metahumanos);
+            ImageView image = (ImageView) view1.findViewById(R.id.imgMeta);
+
+            name.setText(namePlanets[position]);
+            image.setImageResource(imagePlanets[position]);
+
+
+            return view1;
+        }
+    }
+}
